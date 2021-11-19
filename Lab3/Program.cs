0b7c7c0fcef746c7ba115e8895d2f7a3de2cd5b6
79064 <79064@MSI>
2021-11-19 12:57:33 +0300

    Добавьте файлы проекта.

---
+++ Lab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите координаты прямоугольника");
+            Console.WriteLine("Введите координаты x1, y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координаты x2, y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координаты x3, y3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            int x4 = 0;
+            int y4 = 0;
+            if (x1==x2)
+            {
+                if (y3 == y2)
+                {
+                    y4 = y1;
+                    x4 = x3;
+                }
+                else if (y3 == y1)
+                {
+                    x4 = x3;
+                    y4 = y2;
+                }
+                else Console.WriteLine("стороны не параллельны координатным осям");
+            }
+            else Console.WriteLine("стороны не параллельны координатным осям");
+            Console.WriteLine("х1={0}; y1={1}", x1, y1);
+            Console.WriteLine("х2={0}; y2={1}", x1, y1);
+            Console.WriteLine("х3={0}; y3={1}", x1, y1);
+            Console.WriteLine("х4={0}; y4={1}", x1, y1);
+            Console.WriteLine("Для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProjectSwitch/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст от 20 до 69");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a/10)
+            {
+                case 2:
+                    {
+                        Console.Write("Двадцать ");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.Write("Тридцать ");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.Write("Сорок ");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.Write("Пятьдесят ");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.Write("Шестьдесят ");
+                        break;
+                    }
+            }
+            switch (a%10)
+            {
+                case 1:
+                    {
+                        Console.Write("один ");
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Write("два" );
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.Write("три ");
+                        break;
+                    }
+                case 4:
+                    {
+                        Console.Write("четыре ");
+                        break;
+                    }
+                case 5:
+                    {
+                        Console.Write("пять ");
+                        break;
+                    }
+                case 6:
+                    {
+                        Console.Write("шесть ");
+                        break;
+                    }
+                case 7:
+                    {
+                        Console.Write("семь ");
+                        break;
+                    }
+                case 8:
+                    {
+                        Console.Write("восемь ");
+                        break;
+                    }
+                case 9:
+                    {
+                        Console.Write("девять ");
+                        break;
+                    }
+            }
+            if ((a % 10) == 1)
+                Console.WriteLine("год");
+            if (((a % 10) >=2) && (a % 10) <=4)
+                Console.WriteLine("года");
+            if (((a % 10) ==0) || ((a % 10) >=5) && ((a % 10) <= 9))
+                Console.WriteLine("лет");
+            Console.Write("для завершения нажмите любую клавишу");
+            Console.ReadKey();
+        }
+    }
+}
